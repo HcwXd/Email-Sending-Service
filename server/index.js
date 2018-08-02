0038d649cb764c8987d0329612659f77e0512cd4
HcwXd <e04dl47878@gmail.com>
2018-08-02 10:27:40 +0800

    Finish Ouath before Database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
-// Client ID
-// 751544394821-8irfoasluqf0o2hkfrh3m276m80a6lup.apps.googleusercontent.com
-// Client Secret
-// 9jatWuHjG3auB_BRhHuP9f3h
+const express = require('express');
+
+const app = express();
+require('./services/passport');
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
