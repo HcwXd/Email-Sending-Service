c7606e40f69705f8bab910fe9e265a2bc5b8c71c
HcwXd <e04dl47878@gmail.com>
2018-08-08 02:19:41 +0800

    Redux setup

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-ReactDOM.render(<App />, document.querySelector('#root'));
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+// createStore(reducer, initialState, middleware)
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
+);
