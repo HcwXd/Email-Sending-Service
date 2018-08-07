fc247b8c9527e15d60edd73990007fc657726589
HcwXd <e04dl47878@gmail.com>
2018-08-08 02:28:11 +0800

    React Router Setup

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Landing = () => <h2> Landing </h2>;
+const SurveyNew = () => <h2> SurveyNew </h2>;
 
 class App extends React.Component {
     render() {
-        return <h1>Hello!</h1>;
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Route exact path="/" component={Landing} />
+                        <Route path="/survey" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
     }
 }
 
