b9e411292199bc9de0ecd9e4a323b45308f87d5d
HcwXd <e04dl47878@gmail.com>
2018-08-08 02:34:19 +0800

    Proxy for fetching API (React-thunk)

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -5,10 +5,11 @@ import App from './components/App';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 // createStore(reducer, initialState, middleware)
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
     <Provider store={store}>
