59f101a408d32c49401db38521fd5c910c22e715
HcwXd <e04dl47878@gmail.com>
2018-08-17 16:21:29 +0800

    Requiring Auth

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,7 +1,7 @@
 const keys = require('../config/keys');
 const stripe = require('srtipe')(keys.stripeSecretKey);
 module.exports = (app) => {
-    app.post('/api/stripe', async (req, res) => {
+    app.post('/api/stripe', requireLogin, async (req, res) => {
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
