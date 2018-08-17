c3e47e482e73d56de4c603166c77285636f72254
HcwXd <e04dl47878@gmail.com>
2018-08-17 16:28:38 +0800

    Fix typo bugs

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,5 +1,7 @@
 const keys = require('../config/keys');
-const stripe = require('srtipe')(keys.stripeSecretKey);
+const stripe = require('stripe')(keys.stripeSecretKey);
+
+const requireLogin = require('../middlewares/requireLogin');
 module.exports = (app) => {
     app.post('/api/stripe', requireLogin, async (req, res) => {
         const charge = await stripe.charges.create({
