fe84a1233e7924c9f15c58e8207e0010bb478729
HcwXd <e04dl47878@gmail.com>
2018-08-17 15:38:16 +0800

    Env Variable in React

---
+++ server/config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID: '751544394821-8irfoasluqf0o2hkfrh3m276m80a6lup.apps.googleusercontent.com',
+    googleClientSecret: 'mT7RXCuKjFnwszNCSXCl96uO',
+    mongoURL: 'mongodb://username:password123@ds161391.mlab.com:61391/emaily',
+    cookieKey: 'secret',
+    sendGridKey: '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',
+    stripePublishableKey: 'pk_test_x51ZkXX0U0zQnsLHii24DKu0',
+    stripeSecretKey: 'sk_test_ZsJ04gMH0pdzPDGwnDFMTRey',
+};
