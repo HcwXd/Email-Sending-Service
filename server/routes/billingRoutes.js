e9455d6b1cbde09e48ca808bc278edae8989b010
HcwXd <e04dl47878@gmail.com>
2018-08-17 16:18:17 +0800

    Create Charge Object

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,20 +1,12 @@
-const passport = require('passport');
-
+const keys = require('../config/keys');
+const stripe = require('srtipe')(keys.stripeSecretKey);
 module.exports = (app) => {
-    app.use(
-        '/auth/google',
-        passport.authenticate('google', {
-            scope: ['profile', 'email'],
-        })
-    );
-    app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
-        res.redirect('/surveys');
-    });
-    app.get('/api/current_user', (req, res) => {
-        res.send(req.user);
-    });
-    app.get('/api/logout', (req, res) => {
-        req.logout();
-        res.redirect('/surveys');
+    app.post('/api/stripe', async (req, res) => {
+        const charge = await stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            description: '$5 for 5 credits',
+            source: req.body.id,
+        });
     });
 };
