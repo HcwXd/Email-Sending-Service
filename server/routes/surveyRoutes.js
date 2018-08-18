8469091af1b2595ba2d86b61a35e1fd3f96d16c4
HcwXd <e04dl47878@gmail.com>
2018-08-18 15:39:35 +0800

    Polish in the route handler

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -5,7 +5,7 @@ const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplate/surveyTemplate');
 
 module.exports = (app) => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
         const survey = new Survey({
             title,
@@ -17,6 +17,14 @@ module.exports = (app) => {
         });
         const mailer = new Mailer(survey, surveyTemplate(template));
 
-        mailer.send();
+        try {
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
 };
