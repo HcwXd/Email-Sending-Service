1707ecc18caea38d85f328742175950d6b13e47b
HcwXd <e04dl47878@gmail.com>
2018-08-18 14:56:03 +0800

    Survey Model

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURL);
