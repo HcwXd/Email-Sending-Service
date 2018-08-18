784284590aa0c5aeea758df6e12d5deddfbabe64
HcwXd <e04dl47878@gmail.com>
2018-08-19 05:47:02 +0800

    Parsing the Route

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -4,6 +4,10 @@ const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplate/surveyTemplate');
 
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
+
 module.exports = (app) => {
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
@@ -26,6 +30,17 @@ module.exports = (app) => {
         } catch (err) {
             res.status(422).send(err);
         }
+    });
+
+    app.post('api/surveys/webhook', (req, res) => {
+        const event = _.map(req.body, ({ email, url }) => {
+            const pathname = new URL(url).pathname;
+            const p = new Path('/api/surveys/:surveyId/:choice');
+            const match = p.test(pathname);
+            if (match) {
+                return { email, surveyId: match.surveyId, choice: match.choice };
+            }
+        });
     });
 
     app.get('/api/survey/thanks', (req, res) => {
