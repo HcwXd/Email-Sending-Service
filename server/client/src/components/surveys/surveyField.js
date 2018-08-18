53311fc0c21257721d570b67986d82ed3eb469e7
HcwXd <e04dl47878@gmail.com>
2018-08-18 18:15:20 +0800

    Form Validation

--- server/client/src/components/surveys/surveyField.js
+++ server/client/src/components/surveys/surveyField.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default ({ input, label }) => {
+export default ({ input, label, meta: { error, touched } }) => {
     return (
-        <div>
+        <div className="red-text" style={{ marinBottom: '20px', marginTop: '5px' }}>
             <label>{label}</label>
             <input {...input} />
+            {touched && error}
         </div>
     );
 };
